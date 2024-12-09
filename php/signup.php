e00be67c1f803946f09a797921bf6c99780cd00e
Mohamedbs456 <bsalemmed456@gmail.com>
2024-12-09 23:34:41 +0100

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -5,16 +5,14 @@ include('db.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash password for security
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
 
-    // Check if the email or username already exists
     $sql = "SELECT * FROM user WHERE email = '$email' OR username = '$username'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         echo "<script>alert('User with this email or username already exists.');</script>";
         echo "<script>window.location.href = '../html/signup.html';</script>";
     } else {
-        // Insert the new user into the database
         $sql = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$password')";
         if ($conn->query($sql) === TRUE) {
             echo "<script>alert('New record created successfully.!');</script>";
