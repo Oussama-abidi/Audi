4cce05b7c903c8a8cecb6639cb8fa124fd5ff84c
Mohamedbs456 <bsalemmed456@gmail.com>
2024-12-09 23:34:16 +0100

    Update process_order.php

--- php/process_order.php
+++ php/process_order.php
@@ -6,16 +6,14 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
-// Process the order
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $car_model = $_POST['car_model'];
     $quantity = $_POST['quantity'];
 
-    // Save the order to the database (you can add more fields like delivery address, etc.)
-    include('db.php'); // Make sure to include the database connection
+    include('db.php');
 
-    $user_id = $_SESSION['user_id']; // Get the logged-in user ID
-    $order_date = date('Y-m-d H:i:s'); // Get the current date and time
+    $user_id = $_SESSION['user_id']; 
+    $order_date = date('Y-m-d H:i:s'); 
 
     $sql = "INSERT INTO orders (user_id, car_model, quantity, order_date) 
             VALUES ('$user_id', '$car_model', '$quantity', '$order_date')";
